7995b0c9f71a48e7e463710fa78c7ed7264286ac
Amanreet Bajwa <abajwa@mines.edu>
2011-01-26 21:39:11 -0700

    Finished Chapter 9 - Ajax

--- project02/app/models/cart.rb
+++ project02/app/models/cart.rb
@@ -10,26 +10,18 @@ class Cart
 		if current_item
 			current_item.increment_quantity
 		else
-<<<<<<< HEAD
 			current_item = CartItem.new(product)
 			@items << current_item
 		end
 		current_item
-=======
-			@items << CartItem.new(product)
-		end
->>>>>>> 868e3b4fb6ae6ada719128af5d841561815cc2bc
 	end
 	
 	def total_price
 		@items.sum { |item| item.price }
 	end
-<<<<<<< HEAD
 	
 	def total_items
 		@items.sum { |item| item.quantity }
 	end
-=======
->>>>>>> 868e3b4fb6ae6ada719128af5d841561815cc2bc
 end
 
