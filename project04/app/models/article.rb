06307c8839bd6b7d9e35ec8083de7ae00ddbe898
Amanreet Bajwa <abajwa@mines.edu>
2011-02-02 10:54:39 -0700

    created application and database

---
+++ project04/app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ActiveRecord::Base
+	# makes sure every field has something in it before submitting
+	validates_presence_of :title, :author_name, :body
+end
---
+++ project04/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'bdd8a613a8717f0d46c5a137c420f1717eea4409d925f773d829be52b72db7e8d32203d25fb8315598838875b80f225ae6bbdd19ad32fb9db5173af3f0e5947b';
