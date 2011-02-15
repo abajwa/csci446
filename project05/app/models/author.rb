52ac1ad68be3c78de21f3b08d5d679ac84f03b0e
Amanreet Bajwa <abajwa@mines.edu>
2011-02-14 21:59:06 -0700

    Fixed gitignore file - hopefully

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,10 +1,14 @@
 class Author < ActiveRecord::Base
 	belongs_to :articles
-	validates_presence_of :author_name, :photo_url;
+	validates_presence_of :author_name
 	validate :author_cannot_be_sally
+	has_attached_file :photo
+
 	
 	protected
+	
 	def author_cannot_be_sally
 		errors.add(:author_name, 'cannot be Sally') if author_name.match(/sally/i)
 	end
+	
 end
--- project05/db/schema.rb
+++ project05/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110214061932) do
+ActiveRecord::Schema.define(:version => 20110215040647) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(:version => 20110214061932) do
     t.string   "photo_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
